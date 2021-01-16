59e62d8e3f88a54b99a53c42ea86a6191d9c7daf
Alexander Ponomarenko <madi-al@yandex.ru>
2021-01-17 00:49:51 +0300

    PR10 - API in progress

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,33 +1,39 @@
 import React from 'react';
+import {api} from '../../utils/api';
 import load from '../../images/editAvatar.svg';
 import profileEdit from '../../images/profile__edit.svg';
 import buttonPlus from '../../images/add-button__plus.svg';
 
 function Main(props) {
 
-    return (
-        <>
-            <main className="content">
-                <section className="profile">
-                    <div className="profile__avatar-container">
-                        <img src="" alt="Аватар" className="profile__avatar" />
-                        <img src={load} alt="Редактирование аватара" onClick={props.onEditAvatar} className="profile__avatar-load" />
-                    </div>
-                    <div className="profile__info">
-                        <h1 className="profile__name">Жак-Ив Кусто</h1>
-                        <button type="button" onClick={props.onEditProfile} className="button"><img src={profileEdit} alt="Изм."
-                            className="profile__edit" /></button>
-                        <p className="profile__description">Исследователь океана</p>
-                    </div>
-                    <button type="button" className="button button_add_card"><img src={buttonPlus} alt="Добавить"
-                        onClick={props.onAddPlace} className="profile__add-button" /></button>
-                </section>
+  //Хуки начальный данных профайла//
+  const [userName, setUserName] = React.useState();
+  const [userDescription, setUserDescription] = React.useState();
+  const [userAvatar, setUserAvatar] = React.useState();
 
-                <section className="elements"></section>
+  return (
+    <>
+      <main className="content">
+        <section className="profile">
+          <div className="profile__avatar-container">
+            <div style={{ backgroundImage: `url(${userAvatar})` }}  alt="Аватар" className="profile__avatar"></div>
+            <img src={load} alt="Редактирование аватара" onClick={props.onEditAvatar} className="profile__avatar-load" />
+          </div>
+          <div className="profile__info">
+            <h1 className="profile__name">{userName}</h1>
+            <button type="button" onClick={props.onEditProfile} className="button"><img src={profileEdit} alt="Изм."
+              className="profile__edit" /></button>
+            <p className="profile__description">{userDescription}</p>
+          </div>
+          <button type="button" className="button button_add_card"><img src={buttonPlus} alt="Добавить"
+            onClick={props.onAddPlace} className="profile__add-button" /></button>
+        </section>
 
-            </main>
-        </>
-    );
+        <section className="elements"></section>
+
+      </main>
+    </>
+  );
 }
 
 export default Main;
